88bffe967296f70f24190eaa96745f5ef77668d0
zmenzie <zmenzie@purdue.edu>
2020-11-09 22:53:56 -0500

    final touches

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from '../model.question';
 import { MainService } from '../main.service';
-import { Option } from '../model.option';
 
 @Component({
   selector: 'app-main',
@@ -12,18 +11,46 @@ export class MainComponent implements OnInit {
 
   constructor(private mainService: MainService) { }
 
+  // Question data from JSON:
   questionInfo: Question[];
-  options: Option[];
-  // Current question:
-  i = 0;
+
+  // All logged answers:
+  isCorrect: boolean[] = [false, false, false, false, false];
+  selected: number[] = [-1, -1, -1, -1, -1];
+
+  // Current question and index:
+  q: Question;
+  i: number = 0;
+
+  // Total number of questions:
+  length: number = 0;
+
+  // Page checker:
+  page: string = "quiz";
+
+  // Final score
+  score: number = 0;
 
   ngOnInit(): void {
-    this.mainService.loadQuestionDetails().subscribe(data => this.questionInfo = data);
+    this.mainService.loadQuestionDetails().subscribe(data => {
+      this.questionInfo = data;
+      // Do not write above this line
+      for (let x of Object.keys(this.questionInfo)) {
+        length++;
+      }
+      console.log(length);
+
+    });
     console.log('Data loaded!');
   }
 
-  answerSelected(): void {
-
+  answerSelected(isAnswer: boolean, oid: number): void {
+    console.log(isAnswer);
+    this.isCorrect[this.i] = isAnswer;
+    this.selected[this.i] = oid;
+    if (isAnswer) {
+      this.score++;
+    }
   }
 
   nextQuestion(): void {
@@ -34,13 +61,34 @@ export class MainComponent implements OnInit {
   }
 
   prevQuestion(): void {
-    if (this.i < 0) {
+    if (this.i > 0) {
       this.i--;
     }
     console.log(this.i);
   }
 
   submit(): void {
+    for (let a of this.isCorrect) {
+      console.log(a);
+    }
+    // Set page
+    this.page = "submit";
+  }
+  
+  tryAgain() {
+    // Reset all answers to false
+    for (let j in this.isCorrect) {
+      this.isCorrect[j] = false;
+      this.selected[j] = -1;
+    }
+    console.log(this.isCorrect);
+    console.log(this.selected);
 
+    // Set page
+    this.page = "quiz";
+    // Set question
+    this.i = 0;
+    // Reset score
+    this.score = 0;
   }
 }
--- src/app/model.option.ts
+++ src/app/model.option.ts
@@ -1,8 +1,10 @@
 export class Option {
+    oid: number;
     text: string;
     isAnswer: boolean;
     constructor(data: any) {
         data = data || {}; 
+        this.oid = data.oid;
         this.text = data.text;
         this.isAnswer = data.isAnswer;
     }
